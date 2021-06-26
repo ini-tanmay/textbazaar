38645723054733f8545545e556fbb12534e2e929
cmd-dev <tanmay.armal@somaiya.edu>
2021-06-27 01:07:16 +0530

    fixed worker.py

--- writer/worker.py
+++ writer/worker.py
@@ -1,15 +1,16 @@
 import os
 import urllib.parse
-
 import redis
+from redis import Redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL')
+redis_url = 'redis://redistogo:4d234d8e4f0faae5f701ef2c1b992926@spinyfin.redistogo.com:9064/'
+# redis_url = os.getenv('REDISTOGO_URL')
 
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
+urllib.parse.uses_netloc.append('redis')
+url = urllib.parse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
