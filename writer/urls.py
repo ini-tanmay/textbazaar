4e84dc7bf8e65ef4178a48924db2d19245b807d8
cmd-dev <tanmay.armal@somaiya.edu>
2021-06-27 12:34:29 +0530

    added 50% static files

--- writer/urls.py
+++ writer/urls.py
@@ -3,5 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('',index),
+    path('pricing',pricing),
+    path('dashboard',panel),
+    # path('payment',plan_payment),
+    # path('credits_payment',credits_payment),
+    # path('success',payment_success,name='success'),
+    path('signup',register),
+    path('logout',logout_user),
     path('query/<str:query>/<str:email>',query_page),
 ]
--- writer/views.py
+++ writer/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
 from .helpers import * 
 from .email import *
 
 
 def index(request):
     return render(request,'writer/index.html')
+
+def pricing(request):
+    return render(request,'writer/pricing.html')
+
+def panel(request):
+    return render(request,'writer/dashboard.html')
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/pricing")
+    else:
+        form = NewUserForm()
+    return render(request,'news/register.html',{'form':form})
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+
 
 
 def query_page(request,query,email):
